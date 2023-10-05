88106a794f832b0e09cdd83022151918bb219be5
Blandin Ilann <blandin.ilann@gmail.com>
2023-10-05 09:18:46 +0200

    Navigation

--- public/resources/js/navigation.js
+++ public/resources/js/navigation.js
@@ -1,5 +1,6 @@
 function toggleTravel(element) {
     element.classList.toggle("slds-is-open");
-    
+    let open = element.classList.contains("slds-is-open");
+    element.innerHTML = element.innerHTML.replace((open ? "chevrondown" : "chevronup"), (open ? "chevronup" : "chevrondown"));
 }
 
