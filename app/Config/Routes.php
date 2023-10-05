e5ea179096708caf193d2999cfa01a845020616e
pauline bertrand <pauline.bertrand.461@gmail.com>
2023-10-05 09:36:20 +0200

    Alias root

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -18,41 +18,41 @@ use CodeIgniter\Router\RouteCollection;
 $routes->group("/travel", function (RouteCollection $routes) {
 
     $routes->group("/model", function (RouteCollection $routes) {
-        $routes->get("/", "ModelTravelController::viewList");
-        $routes->get("/add", "ModelTravelController::viewAdd");
-        $routes->get("/(:num)", "ModelTravelController::viewDetail/$1");
-        $routes->get("/(:num)/edit", "ModelTravelController::viewEdit/$1");
-        $routes->get("/(:num)/delete", "ModelTravelController::delete/$1");
+        $routes->get("/", "ModelTravelController::viewList", ["as" => "modelTravelList"]);
+        $routes->get("/add", "ModelTravelController::viewAdd", ["as" => "modelTravelViewAdd"]);
+        $routes->get("/(:num)", "ModelTravelController::viewDetail/$1", ["as" => "modelTravelViewDetail"]);
+        $routes->get("/(:num)/edit", "ModelTravelController::viewEdit/$1", ["as" => "modelTravelViewEdit"]);
+        $routes->get("/(:num)/delete", "ModelTravelController::delete/$1", ["as" => "modelTravelDelete"]);
     });
 
-    $routes->get("/", "TravelController::viewList");
-    $routes->get("/add", "TravelController::viewAdd");
-    $routes->get("/(:num)", "TravelController::viewDetail/$1");
-    $routes->get("/(:num)/edit", "TravelController::viewEdit/$1");
-    $routes->get("/(:num)/delete", "TravelController::delete/$1");
+    $routes->get("/", "TravelController::viewList", ["as" => "modelTravelViewList"]);
+    $routes->get("/add", "TravelController::viewAdd", ["as" => "modelTravelViewAdd"]);
+    $routes->get("/(:num)", "TravelController::viewDetail/$1", ["as" => "modelTravelViewDetail"]);
+    $routes->get("/(:num)/edit", "TravelController::viewEdit/$1", ["as" => "modelTravelViewEdit"]);
+    $routes->get("/(:num)/delete", "TravelController::delete/$1", ["as" => "modelTravelDelete"]);
 });
 
 /*
  * Regroupement de routes pour les avis clients (reviews)
  */
 $routes->group("/reviews", function (RouteCollection $routes) {
-    $routes->get("/", "ReviewController::viewList");
-    $routes->get("/add", "ReviewController::viewAdd");
-    $routes->get("/(:num)", "ReviewController::viewDetail/$1");
+    $routes->get("/", "ReviewController::viewList", ["as" => "ReviewControllerViewList"]);
+    $routes->get("/add", "ReviewController::viewAdd", ["as" => "ReviewControllerViewAdd"]);
+    $routes->get("/(:num)", "ReviewController::viewDetail/$1", ["as" => "ReviewControllerViewDetail"]);
 });
 
-$routes->get("/", "HomeController::view");
-$routes->get("/login", "LoginController::view");
+$routes->get("/", "HomeController::view", ["as" => "HomeControllerView"]);
+$routes->get("/login", "LoginController::view", ["as" => "LoginControllerView"]);
 
 /*
  * --------------------------------------------------------------------
  * Routes de retour de formulaire
  * --------------------------------------------------------------------
  */
-$routes->post("/login", "LoginController::login");
-$routes->get("/logout", "LoginController::logout");
-$routes->post("/model/travel/add", "ModelTravelController::add");
-$routes->post("/model/travel/(:num)/edit", "ModelTravelController::edit/$1");
-$routes->post("/travel/add", "TravelController::add");
-$routes->post("/travel/(:num)/edit", "TravelController::edit/$1");
-$routes->post("/reviews/add", "ReviewController::add");
+$routes->post("/login", "LoginController::login", ["as" => "LoginControllerLogin"]);
+$routes->get("/logout", "LoginController::logout", ["as" => "LoginControllerLogout"]);
+$routes->post("/model/travel/add", "ModelTravelController::add", ["as" => "ModelTravelControllerAdd"]);
+$routes->post("/model/travel/(:num)/edit", "ModelTravelController::edit/$1", ["as" => "ModelTravelControllerEdit"]);
+$routes->post("/travel/add", "TravelController::add", ["as" => "TravelControllerAdd"]);
+$routes->post("/travel/(:num)/edit", "TravelController::edit/$1", ["as" => "TravelControllerEdit"]);
+$routes->post("/reviews/add", "ReviewController::add", ["as" => "ReviewControllerAdd"]);
