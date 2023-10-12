e3d20ddd3cb9d143eaac8322697f724d3b99d258
pauline bertrand <pauline.bertrand.461@gmail.com>
2023-10-12 09:18:05 +0200

    modif

--- app/Views/pages/travel/list.php
+++ app/Views/pages/travel/list.php
@@ -12,7 +12,7 @@
 
     <div class="slds-box" style="width: 95% !important; background-color: white; margin: auto; margin-top: 20px;">
         <div style="display: flex; flex-direction: row; justify-content: space-between;">
-            <h2 class="slds-text-heading_medium">Liste d'instance de voyages</h2>
+            <h2 class="slds-text-heading_medium">Liste d'instances de voyages</h2>
             <a href="<?= url_to("travelViewAdd") ?>" class="slds-button slds-button_brand">Ajouter une instance de voyage</a>
         </div>
         <hr style="margin: 30px 0;">
