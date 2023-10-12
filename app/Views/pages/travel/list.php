7c8a7955770186b00c313597f647d61131da95a4
pauline bertrand <pauline.bertrand.461@gmail.com>
2023-10-12 09:06:59 +0200

    travel view add

--- app/Views/pages/travel/list.php
+++ app/Views/pages/travel/list.php
@@ -13,7 +13,7 @@
     <div class="slds-box" style="width: 95% !important; background-color: white; margin: auto; margin-top: 20px;">
         <div style="display: flex; flex-direction: row; justify-content: space-between;">
             <h2 class="slds-text-heading_medium">Liste d'instance de voyages</h2>
-            <a href="" class="slds-button slds-button_brand">Ajouter une instance de voyage</a>
+            <a href="<?= url_to("travelViewAdd") ?>" class="slds-button slds-button_brand">Ajouter une instance de voyage</a>
         </div>
         <hr style="margin: 30px 0;">
         <table id="travel-datatable"></table>
