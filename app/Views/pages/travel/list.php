90c8f6eb6d80ccd7cbd785bb7b385bf943769a6f
pauline bertrand <pauline.bertrand.461@gmail.com>
2023-10-12 08:31:57 +0200

    travel instance list

--- app/Views/pages/travel/list.php
+++ app/Views/pages/travel/list.php
@@ -1,9 +1,22 @@
 <?= $this->extend("_pattern") ?>
 
+<?= $this->section("assets") ?>
+    <script src="<?= base_url("resources/js/travel/list.js") ?>"></script>
+<?= $this->endSection() ?>
+
 <?= $this->section("main") ?>
 
     <?= $this->include("templates/navigation") ?>
 
-    <p>TRAVEL LIST</p>
+    <div id="data" style="display: none;"><?= json_encode($models) ?></div>
+
+    <div class="slds-box" style="width: 95% !important; background-color: white; margin: auto; margin-top: 20px;">
+        <div style="display: flex; flex-direction: row; justify-content: space-between;">
+            <h2 class="slds-text-heading_medium">Liste d'instance de voyages</h2>
+            <a href="" class="slds-button slds-button_brand">Ajouter une instance de voyage</a>
+        </div>
+        <hr style="margin: 30px 0;">
+        <table id="travel-datatable"></table>
+    </div>
 
 <?= $this->endSection() ?>
