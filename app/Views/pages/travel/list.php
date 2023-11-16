985bb00706fa183fe9fac53f51a14eebd1ec6358
pauline bertrand <pauline.bertrand.461@gmail.com>
2023-11-16 10:22:50 +0100

    titres

--- app/Views/pages/travel/list.php
+++ app/Views/pages/travel/list.php
@@ -12,8 +12,8 @@
 
     <div class="slds-box" style="width: 95% !important; background-color: white; margin: auto; margin-top: 20px;">
         <div style="display: flex; flex-direction: row; justify-content: space-between;">
-            <h2 class="slds-text-heading_medium">Liste d'instances de voyages</h2>
-            <a href="<?= url_to("travelViewAdd") ?>" class="slds-button slds-button_brand">Ajouter une instance de voyage</a>
+            <h2 class="slds-text-heading_medium">Liste des voyages</h2>
+            <a href="<?= url_to("travelViewAdd") ?>" class="slds-button slds-button_brand">Ajouter un voyage</a>
         </div>
         <hr style="margin: 30px 0;">
         <table id="travel-datatable"></table>
